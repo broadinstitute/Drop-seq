de9fa9e738760f00b636d0c2b9082ece55fcd320
nemesh <nemesh@broadinstitute.org>
2015-10-01 09:34:05 -0400

    Changed UmiIteratorWrapper ctor to take an Iterator instead of a CloseableIterator.  It's not like the wrapper ever calls close.

--- src/java/org/broadinstitute/dropseqrna/utils/readiterators/UmiIteratorWrapper.java
+++ src/java/org/broadinstitute/dropseqrna/utils/readiterators/UmiIteratorWrapper.java
@@ -1,18 +1,19 @@
 package org.broadinstitute.dropseqrna.utils.readiterators;
 
 import htsjdk.samtools.SAMRecord;
-import htsjdk.samtools.util.CloseableIterator;
-import org.broadinstitute.dropseqrna.barnyard.Utils;
-import org.broadinstitute.dropseqrna.utils.CountChangingIteratorWrapper;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Random;
 import java.util.Set;
 
+import org.broadinstitute.dropseqrna.barnyard.Utils;
+import org.broadinstitute.dropseqrna.utils.CountChangingIteratorWrapper;
+
 //TODO: Better name?
 public class UmiIteratorWrapper extends CountChangingIteratorWrapper<SAMRecord> {
-	
+
 	private String cellBarcodeTag;
 	private Set<String> cellBarcodeList;
 	private String geneExonTag;
@@ -21,7 +22,7 @@ public class UmiIteratorWrapper extends CountChangingIteratorWrapper<SAMRecord> 
 	private boolean assignReadsToAllGenes;
 	private boolean useStrandInfo;
 	private Random rand = new Random();
-	
+
 	/**
 	 * Filters and copies reads for generating UMICollections.
 	 * Only accepts reads that where the read cell barcode matches a barcode in the list (if not null)
@@ -37,14 +38,14 @@ public class UmiIteratorWrapper extends CountChangingIteratorWrapper<SAMRecord> 
 	 * @param assignReadsToAllGenes Clone SAMRecord for each gene
 	 * @param useStrandInfo Only queue SAMRecord if gene strand agrees with read strand.
 	 */
-	public UmiIteratorWrapper(CloseableIterator<SAMRecord> underlyingIterator,
-                              String cellBarcodeTag,
-                              Collection<String> cellBarcodeList,
-                              String geneExonTag,
-                              String strandTag,
-                              int readMQ,
-                              boolean assignReadsToAllGenes,
-                              boolean useStrandInfo) {
+	public UmiIteratorWrapper(final Iterator<SAMRecord> underlyingIterator,
+                              final String cellBarcodeTag,
+                              final Collection<String> cellBarcodeList,
+                              final String geneExonTag,
+                              final String strandTag,
+                              final int readMQ,
+                              final boolean assignReadsToAllGenes,
+                              final boolean useStrandInfo) {
         super(underlyingIterator);
 		this.cellBarcodeTag = cellBarcodeTag;
 		this.cellBarcodeList = new HashSet<String>(cellBarcodeList);
@@ -56,47 +57,42 @@ public class UmiIteratorWrapper extends CountChangingIteratorWrapper<SAMRecord> 
 	}
 
     @Override
-    protected void processRecord(SAMRecord r) {
+    protected void processRecord(final SAMRecord r) {
 		String cellBC=r.getStringAttribute(cellBarcodeTag);
 		String geneList = r.getStringAttribute(this.geneExonTag);
-		
+
 		// if there are cell barcodes to filter on, and this read's cell barcode isn't one of them, then move on to the next read;
-		if (this.cellBarcodeList!=null && !cellBarcodeList.contains(cellBC)) {
+		if (this.cellBarcodeList!=null && !cellBarcodeList.contains(cellBC))
 			return;
-		}
 		// if the read doesn't pass map quality, etc, more on.
-		if (r.isSecondaryOrSupplementary() || r.getMappingQuality()<this.readMQ || geneList==null) {
+		if (r.isSecondaryOrSupplementary() || r.getMappingQuality()<this.readMQ || geneList==null)
 			return;
-		}
-		
+
 		// there's at least one good copy of the read.  Does the read match on strand/gene, or is it assigned to multiple genes?
 		String [] genes = geneList.split(",");
 		String [] strands = null;
-		
-		if (this.useStrandInfo) {
+
+		if (this.useStrandInfo)
 			strands = r.getStringAttribute(strandTag).split(",");
-		}
-		
-		if (this.assignReadsToAllGenes) {
+
+		if (this.assignReadsToAllGenes)
 			for (int i=0; i<genes.length; i++) {
 				String g = genes[i];
 				SAMRecord rr = Utils.getClone(r);
 				rr.setAttribute(geneExonTag, g);
-				
+
 				// if you use strand info, then the gene has to match the read strand for the read to be added.
 				if (useStrandInfo) {
 					String geneStrand = strands[i];
 					String readStrandString = Utils.strandToString(!r.getReadNegativeStrandFlag());
-					if (geneStrand.equals(readStrandString)) {
-                        queueRecordForOutput(rr);
-					} else {
+					if (geneStrand.equals(readStrandString))
+						queueRecordForOutput(rr);
+					else {
 						// rejected read
 					}
-				} else { // if you don't use strand info, add the read to all genes.
-                    queueRecordForOutput(rr);
-				}
+				} else
+					queueRecordForOutput(rr);
 			}
-		} 
 		else {
 			// pick a random read.
 			int randomNum = rand.nextInt((genes.length-1) + 1);
