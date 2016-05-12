9d0faea327c7dd9c4bff00b13df6d35e6124a186
nemesh <nemesh@broadinstitute.org>
2016-05-12 10:50:50 -0400

    Moved from private to public.

---
+++ src/java/org/broadinstitute/dropseqrna/utils/FilterBAMByTag.java
@@ -0,0 +1,216 @@
+package org.broadinstitute.dropseqrna.priv.utils;
+
+import htsjdk.samtools.SAMFileWriter;
+import htsjdk.samtools.SAMFileWriterFactory;
+import htsjdk.samtools.SAMRecord;
+import htsjdk.samtools.SamReader;
+import htsjdk.samtools.SamReaderFactory;
+import htsjdk.samtools.util.CloserUtil;
+import htsjdk.samtools.util.IOUtil;
+import htsjdk.samtools.util.Log;
+import htsjdk.samtools.util.ProgressLogger;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.broadinstitute.dropseqrna.cmdline.DropSeq;
+import org.broadinstitute.dropseqrna.priv.utils.modularfileparser.DelimiterParser;
+import org.broadinstitute.dropseqrna.priv.utils.modularfileparser.ModularFileParser;
+import org.broadinstitute.dropseqrna.utils.CustomBAMIterators;
+import org.broadinstitute.sv.util.PeekableIterator;
+
+import picard.cmdline.CommandLineProgram;
+import picard.cmdline.CommandLineProgramProperties;
+import picard.cmdline.Option;
+import picard.cmdline.StandardOptionDefinitions;
+
+@CommandLineProgramProperties(usage = "Filters a BAM file based on a TAG and a file containing a list of values.  This is pretty similar to grepping with a file, but is faster and makes a proper BAM.", usageShort = "Filters a BAM file based on a TAG and a file containing a list of values.", programGroup = DropSeq.class)
+public class FilterBAMByTag extends CommandLineProgram {
+
+	private final Log log = Log.getInstance(FilterBAMByTag.class);
+	
+	@Option(shortName = StandardOptionDefinitions.INPUT_SHORT_NAME, doc = "The input SAM or BAM file to analyze.")
+	public File INPUT;
+
+	@Option(shortName = StandardOptionDefinitions.OUTPUT_SHORT_NAME, doc = "Output report")
+	public File OUTPUT;
+
+	@Option(doc = "The BAM tag to filter on.")
+	public String TAG;
+
+	@Option(doc = "A file with 1 column and 1 or more rows containing a barcode value per line.", optional = true)
+	public File TAG_VALUES_FILE;
+
+	@Option(doc = "A single value for filtering reads.  Use instead of TAG_VALUES_FILE.", optional = true)
+	public String TAG_VALUE;
+
+	@Option(doc = "If having a tag value matches the values in the file, accept the read.  If set to false, reject the read.")
+	public Boolean ACCEPT_TAG = true;
+		
+	@Option(doc = "In Paired Read Mode if the tag value is on either read the pair of reads is kept or discarded. This is slower when turned on because "
+			+ "of the need to queryname sort the data, so only turn it on if you need it!")
+	public Boolean PAIRED_MODE=false;
+	
+	
+	
+	@Override
+	protected int doWork() {
+		if (TAG_VALUES_FILE == null && TAG == null) {
+			log.error("You must set either a file of tag values, or a single tag value.");
+			System.exit(1);
+		}
+
+		IOUtil.assertFileIsReadable(INPUT);
+		IOUtil.assertFileIsWritable(OUTPUT);
+		Set<String> values;
+
+		if (this.TAG_VALUES_FILE != null) {
+			IOUtil.assertFileIsReadable(TAG_VALUES_FILE);
+			values = readValues(this.TAG_VALUES_FILE);
+		} else {
+			values = new HashSet<String>();
+			if (this.TAG_VALUE!=null)
+				values.add(this.TAG_VALUE);
+		}
+
+		SamReader in = SamReaderFactory.makeDefault().enable(SamReaderFactory.Option.EAGERLY_DECODE).open(INPUT);
+		SAMFileWriter out = new SAMFileWriterFactory().makeSAMOrBAMWriter(
+				in.getFileHeader(), true, OUTPUT);
+		
+		if (!this.PAIRED_MODE) {
+			processUnpairedMode(in, out, values);
+		} else {
+			processPairedMode(in, out, values);
+		}
+		
+		return 0;
+	}
+	
+	/**
+	 * Just work through the reads one at a time.
+	 * @param in
+	 * @param out
+	 */
+	void processUnpairedMode (SamReader in, SAMFileWriter out, Set<String> values) {
+		ProgressLogger progLog = new ProgressLogger(log);
+		for (final SAMRecord r : in) {
+			progLog.record(r);
+			boolean filterFlag = filterRead(r, this.TAG, values, this.ACCEPT_TAG);
+			if (!filterFlag) {
+				out.addAlignment(r);
+			}
+		}
+		CloserUtil.close(in);
+		out.close();
+	}
+	
+	/**
+	 * 
+	 * @param in
+	 * @param out
+	 * @param values
+	 */
+	void processPairedMode (SamReader in, SAMFileWriter out, Set<String> values) {
+		ProgressLogger progLog = new ProgressLogger(log);
+		PeekableIterator<SAMRecord>  iter = new PeekableIterator<SAMRecord>(CustomBAMIterators.getQuerynameSortedRecords(in));
+		while (iter.hasNext()) {
+			SAMRecord r1 = iter.next();
+			progLog.record(r1);
+			boolean filterFlag1 = filterRead(r1, this.TAG, values, this.ACCEPT_TAG);
+			
+			SAMRecord r2 = null;
+			if (iter.hasNext()) r2 = iter.peek();			
+			
+			// check for r2 being null in case the last read is unpaired.
+			if (r2!=null && r1.getReadName().equals(r2.getReadName())) {
+				// paired read found.
+				progLog.record(r2);
+				r2=iter.next();
+				boolean filterFlag2 = filterRead(r2, this.TAG, values, this.ACCEPT_TAG);
+				if (!filterFlag1 || !filterFlag2) {
+					out.addAlignment(r1);
+					out.addAlignment(r2);
+				}
+			} else { // single read
+				if (!filterFlag1) {
+					out.addAlignment(r1);
+				}
+			}			
+		}
+		CloserUtil.close(in);
+		out.close();
+	}
+	
+	boolean retainByReadNumber (SAMRecord r, int desiredReadNumber) {
+		boolean readPaired = r.getReadPairedFlag();
+		// if the read isn't paired, then there's no read number, so keep it.
+		if (!readPaired) {
+			return true;
+		}
+		// read is paired, is it the first of pair?
+		boolean firstRead = r.getFirstOfPairFlag();		
+		// if you're looking for the first read and this isn't, or looking for the 2nd read and this isn't, then go to the next read.
+		if ((firstRead && desiredReadNumber!=1) || (!firstRead && desiredReadNumber==1)) return (false);
+		return true;
+	}
+
+	boolean filterRead(SAMRecord r, String tag, Set<String> values,
+			boolean acceptFlag) {
+		Object v = r.getAttribute(tag);
+		// if the tag is not set, and you need a TAG set, then filter the read.
+		if (v == null && acceptFlag)
+			return true;
+		// if the tag is not set, and you don't need a TAG set, then keep the
+		// read.
+		if (v == null && !acceptFlag)
+			return false;
+
+		String vv = null;
+
+		if (v instanceof Integer) {
+			Integer o = (Integer) v;
+			vv = Integer.toString(o);
+		} else if (v instanceof String) {
+			vv = (String) v;
+		} else {
+			log.info("WHAT ELSE");
+		}
+
+		// if there are no values to scan, it's a match. Start with that.
+		boolean hasElement = true;
+
+		// if there are values, check to see if this tag matches one.
+		if (values != null && values.size() > 0) {
+			hasElement = values.contains(vv);
+		}
+
+		if ((hasElement & acceptFlag)
+				| (hasElement == false & acceptFlag == false)) {
+			return false;
+		}
+		if ((hasElement == false & acceptFlag)
+				| (hasElement & acceptFlag == false)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private Set<String> readValues(File f) {
+		Set<String> result = new HashSet<String>();
+		ModularFileParser p = new ModularFileParser(new DelimiterParser(","),
+				f, 0);
+		String[] line = null;
+		while ((line = p.readNextLine()) != null) {
+			result.add(line[0]);
+		}
+		p.close();
+		return result;
+	}
+
+	/** Stock main method. */
+	public static void main(final String[] args) {
+		System.exit(new FilterBAMByTag().instanceMain(args));
+	}
+}
---
+++ src/tests/java/org/broadinstitute/dropseqrna/utils/FilterBAMByTagTest.java
@@ -0,0 +1,116 @@
+package org.broadinstitute.dropseqrna.priv.utils;
+
+import htsjdk.samtools.SAMRecord;
+import htsjdk.samtools.SAMRecordSetBuilder;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import junit.framework.Assert;
+
+import org.testng.annotations.Test;
+
+public class FilterBAMByTagTest {
+
+	@Test(enabled = true, groups = { "dropseq", "transcriptome" })
+	public void filterReadTest() {
+		SAMRecord readHasAttribute = new SAMRecord(null);
+		String tag = "XT";
+		readHasAttribute.setAttribute(tag, "1");
+
+		Set<String> values = new HashSet<String>();
+		values.add("1");
+
+		SAMRecord readNoAttribute = new SAMRecord(null);
+
+		FilterBAMByTag t = new FilterBAMByTag();
+		// read has attribute, accept any value, want to retain read.
+		boolean flag1 = t.filterRead(readHasAttribute, tag, null, true);
+		Assert.assertFalse(flag1);
+
+		// read has attribute, accept any value, want to filter read.
+		boolean flag2 = t.filterRead(readHasAttribute, tag, null, false);
+		Assert.assertTrue(flag2);
+
+		// read has attribute, accept certain value, want to retain read.
+		boolean flag3 = t.filterRead(readHasAttribute, tag, values, true);
+		Assert.assertFalse(flag3);
+
+		// read has attribute, accept certain value, want to filter read.
+		boolean flag4 = t.filterRead(readHasAttribute, tag, values, false);
+		Assert.assertTrue(flag4);
+
+		// read does not have attribute, accept any value, want to retain read.
+		boolean flag5 = t.filterRead(readNoAttribute, tag, null, true);
+		Assert.assertTrue(flag5);
+
+		// read does not have attribute, accept any value, want to filter read.
+		boolean flag6 = t.filterRead(readNoAttribute, tag, null, false);
+		Assert.assertFalse(flag6);
+
+		// read does not have attribute, accept certain value, want to retain read.
+		boolean flag7 = t.filterRead(readNoAttribute, tag, values, true);
+		Assert.assertTrue(flag7);
+
+		// read does not have attribute, accept certain value, want to filter read.
+		boolean flag8 = t.filterRead(readNoAttribute, tag, values, false);
+		Assert.assertFalse(flag8);
+
+	}
+
+	/**
+	 * Returns a paired read, first of pair in the first position of the list, 2nd of pair in the 2nd position.
+	 * @return
+	 */
+	private List<SAMRecord> getPairedRead () {
+		List<SAMRecord> result = new ArrayList<SAMRecord> ();
+
+		SAMRecordSetBuilder builder = new SAMRecordSetBuilder();
+		builder.addUnmappedPair("test");
+		Collection<SAMRecord> recs = builder.getRecords();
+
+		for (SAMRecord r: recs) {
+			if (r.getFirstOfPairFlag()) result.add(0, r);
+			if (r.getSecondOfPairFlag()) result.add(1, r);
+		}
+		return (result);
+
+	}
+
+	@Test(enabled = true, groups = { "dropseq", "transcriptome" })
+	public void filterByReadNumberTest() {
+		FilterBAMByTag t = new FilterBAMByTag();
+
+		// record paired and read is 1st
+		List<SAMRecord> recs = getPairedRead ();
+		SAMRecord recFirstPaired = recs.get(0);
+		SAMRecord recSecondPaired = recs.get(1);
+
+		boolean flag1= t.retainByReadNumber(recFirstPaired, 1);
+		boolean flag2= t.retainByReadNumber(recFirstPaired, 2);
+		Assert.assertTrue(flag1);
+		Assert.assertFalse(flag2);
+
+		// record paired and read is 2st
+		recSecondPaired.setProperPairFlag(true);
+		recSecondPaired.setSecondOfPairFlag(true);
+		flag1= t.retainByReadNumber(recSecondPaired, 1);
+		flag2= t.retainByReadNumber(recSecondPaired, 2);
+		Assert.assertTrue(flag2);
+		Assert.assertFalse(flag1);
+
+		// record unpaired and read is 1st
+		SAMRecordSetBuilder builder = new SAMRecordSetBuilder();
+		builder.addUnmappedFragment("foo");
+		SAMRecord recFirstUnPaired = builder.getRecords().iterator().next();
+
+		flag1= t.retainByReadNumber(recFirstUnPaired, 1);
+		flag2= t.retainByReadNumber(recFirstPaired, 2);
+		Assert.assertTrue(flag1);
+		Assert.assertFalse(flag2);
+	}
+
+}
