4c34e6ae0b543c2c0ce52dfd2238e67dbe87a73e
nemesh <nemesh@broadinstitute.org>
2017-02-17 12:36:54 -0500

    continued work, nothing "done" yet.

--- src/java/org/broadinstitute/dropseqrna/utils/editdistance/LevenshteinDistanceResult.java
+++ src/java/org/broadinstitute/dropseqrna/utils/editdistance/LevenshteinDistanceResult.java
@@ -1,7 +1,5 @@
 package org.broadinstitute.dropseqrna.utils.editdistance;
 
-import htsjdk.samtools.util.Log;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
@@ -10,36 +8,40 @@ import java.util.Set;
 
 import org.apache.commons.lang.StringUtils;
 
+import htsjdk.samtools.util.Log;
+
 public class LevenshteinDistanceResult {
 
 	private static final Log log = Log.getInstance(LevenshteinDistanceResult.class);
-	
+
 	private int[][] distance = null;
 	private String stringOne;
 	private String stringTwo;
 	private int deletionCost;
 	private int insertionCost;
 	private int substitutionCost;
-	
-	LevenshteinDistanceResult(String stringOne, String stringTwo, int [] [] distance, int deletionCost, int insertionCost, int substitutionCost) {
-		this.distance = distance;	
+
+	LevenshteinDistanceResult(final String stringOne, final String stringTwo, final int [] [] distance, final int deletionCost, final int insertionCost, final int substitutionCost) {
+		this.stringOne=stringOne;
+		this.stringTwo=stringTwo;
+		this.distance = distance;
 		this.deletionCost= deletionCost;
 		this.insertionCost=insertionCost;
 		this.substitutionCost=substitutionCost;
 	}
-	
+
 	public int getEditDistance () {
 		int l1 = distance.length-1;
 		int l2 = distance[0].length-1;
-		return (this.distance[l1][l2]); 
+		return (this.distance[l1][l2]);
 	}
-	
+
 	/**
-	 * After filling out the edit distance matrix, look at the set of operations.  
+	 * After filling out the edit distance matrix, look at the set of operations.
 	 * Parse out the number of substitutions, and count each as ed+1.  Each PAIR of insertion/deletions counts for 1 ed change.
 	 * Only use this if you set the substitution cost to be 2, and the deletion/insertion costs to be 1.
-	 * If you run this on data that has a different substitution/deletion/insertion costs than the expected [2/1/1] then it re-runs populating the edit distance matrix with those values.  
-	 * @return the edit distance corrected for indels on barcodes. 
+	 * If you run this on data that has a different substitution/deletion/insertion costs than the expected [2/1/1] then it re-runs populating the edit distance matrix with those values.
+	 * @return the edit distance corrected for indels on barcodes.
 	 */
 	public Integer getEditDistanceIndelCorrected() {
 		String [] ops=null;
@@ -47,54 +49,52 @@ public class LevenshteinDistanceResult {
 			log.info("Edit distances I expected were [2/1/1] for substitution,insertion,deletion.  You had [" + this.substitutionCost + "/"+ this.insertionCost + "/" + this.deletionCost+". Recalculating with expected costs.");
 			LevenshteinDistanceResult r = LevenshteinDistance.computeLevenshteinDistanceResult(this.stringOne, this.stringTwo, 1,1,2);
 			ops = getOperations(r.distance);
-		} else {
+		} else
 			ops = getOperations();
-		}
 		// just count the indels
 		int count=0;
-		for (String o: ops) {
+		for (String o: ops)
 			switch (o) {
-				case "S": count+=this.substitutionCost; break;
-				case "I": count+=this.deletionCost; break;
-				case "D": count+=this.insertionCost; break;
+				case "S": count+=2; break;
+				case "I": count+=1; break;
+				case "D": count+=1; break;
 				case "M": count+=0; break;
 			}
-		} 
-		return count/this.substitutionCost;
+		return count/2;
 	}
-	
-	
+
+
 	/**
 	 * Get the set of operations to convert the first string to the second.
 	 * There are actually multiple paths one can take that result in multiple paths.  I'd really like to take all equally valid paths and make a list of list of operations
-	 *   
+	 *
 	 * @return
 	 */
 	public String [] getOperations() {
 		return getOperations(this.distance);
 	}
-	
+
 	/**
 	 * Get the set of operations to convert the first string to the second.
 	 * There are actually multiple paths one can take that result in multiple paths.  I'd really like to take all equally valid paths and make a list of list of operations
-	 *   
+	 *
 	 * @return
 	 */
-	private String [] getOperations (int [] []  distance) {
+	private String [] getOperations (final int [] []  distance) {
 		int l1 = distance.length-1;
 		int l2 = distance[0].length-1;
-		List<String> operations = new ArrayList<String>();
-		
+		List<String> operations = new ArrayList<>();
+
 		int rowIndex=l1;
 		int colIndex=l2;
-		
+
 		while (rowIndex>=0 && colIndex>=0) {
-			if (rowIndex==0 && colIndex==0) break;			
+			if (rowIndex==0 && colIndex==0) break;
 			int current = AdjacentScores.getCurrentScore(rowIndex, colIndex, distance);
 			int diagonal= AdjacentScores.getDiagonalScore(rowIndex, colIndex, distance);
 			int up = AdjacentScores.getUpScore(rowIndex, colIndex, distance);
 			int left = AdjacentScores.getLeftScore(rowIndex, colIndex, distance);
-			
+
 			// I think given an equal path to a deletion or a substitution, a deletion is preferred.
 			//if (diagonal==current & diagonal<=left & diagonal <= up) {
 			if (diagonal==current & diagonal<=left & diagonal <= up) {
@@ -104,12 +104,12 @@ public class LevenshteinDistanceResult {
 			}
 			else if (up<= diagonal & up<=current & left > up) {
 				operations.add("D");
-				
+
 				rowIndex--;
 			}// move diagonal as a sub or match
 			else if (diagonal<= up && diagonal <= left && diagonal < current) {
 				//substitution
-				operations.add("S"); 
+				operations.add("S");
 				rowIndex--;
 				colIndex--;
 			}
@@ -118,61 +118,61 @@ public class LevenshteinDistanceResult {
 				operations.add("I");
 				colIndex--;
 			}
-			
+
 		}
-		
+
 		// flip the ordering so it's start to end.
 		Collections.reverse(operations);
 		String [] result = operations.toArray(new String [operations.size()]);
-		
+
 		return (result);
-		
+
 	}
-	
+
 	/**
 	 * A class that defines the scores at a position in the matrix and adjacent scores
 	 * This is purely made for reuse.
 	 */
 	private static class AdjacentScores {
-		
+
 		@SuppressWarnings("unused")
-		public enum Operation { 
+		public enum Operation {
 			M("M"),S("S"),I("I"),D("D");
-			
+
 			private final String name;
-			Operation (String name) {
+			Operation (final String name) {
 				this.name=name;
 			}
 			public String getName () {
 				return this.name;
 			}
 		}
-		
-		static int getCurrentScore (int rowIndex, int colIndex, int distance [] []) {
+
+		static int getCurrentScore (final int rowIndex, final int colIndex, final int distance [] []) {
 			return distance[rowIndex][colIndex];
 		}
-		
-		static int getUpScore(int rowIndex, int colIndex, int distance [] []) {
-			// have to protect against running into the edges of the matrix.			
-			int up =Math.max(rowIndex, colIndex);			
+
+		static int getUpScore(final int rowIndex, final int colIndex, final int distance [] []) {
+			// have to protect against running into the edges of the matrix.
+			int up =Math.max(rowIndex, colIndex);
 			if (rowIndex>0) up = distance[rowIndex-1][colIndex];
 			return up;
 		}
-		
-		static int getLeftScore(int rowIndex, int colIndex, int distance [] []) {
+
+		static int getLeftScore(final int rowIndex, final int colIndex, final int distance [] []) {
 			// have to protect against running into the edges of the matrix.
 			int left=Math.max(rowIndex, colIndex);
-			if (colIndex>0) left = distance[rowIndex][colIndex-1];			
+			if (colIndex>0) left = distance[rowIndex][colIndex-1];
 			return left;
 		}
-		
-		static int getDiagonalScore(int rowIndex, int colIndex, int distance [] []) {
+
+		static int getDiagonalScore(final int rowIndex, final int colIndex, final int distance [] []) {
 			// have to protect against running into the edges of the matrix.
 			int diagonal=Math.max(rowIndex, colIndex);
-			if (rowIndex>0 && colIndex>0) diagonal = distance[rowIndex-1][colIndex-1];			
+			if (rowIndex>0 && colIndex>0) diagonal = distance[rowIndex-1][colIndex-1];
 			return diagonal;
 		}
-				
+
 	}
 	/**
 	 * This gets all possible sets of edit distance operations along valid paths of decreasing edit distance.
@@ -183,42 +183,41 @@ public class LevenshteinDistanceResult {
 	private List<String [] > getAllOperationsMultiPath () {
 		int l1 = distance.length-1;
 		int l2 = distance[0].length-1;
-		
+
 		// initialize at the bottom corner of the matrix.
 		Operations o = new Operations(l1, l2);
-		List<Operations> searchList = new ArrayList<Operations>();
+		List<Operations> searchList = new ArrayList<>();
 		searchList.add(o);
-		
-		Set<Operations> finalSet = new HashSet<Operations>();
-		
-		
+
+		Set<Operations> finalSet = new HashSet<>();
+
+
 		// start at the bottom corner of the matrix, and loop until all OperationList objects have climbed to 0,0.
 		while (!searchList.isEmpty()) {
-			List<Operations> searchListNext = new ArrayList<Operations>();
-			for (Operations ol: searchList) {
+			List<Operations> searchListNext = new ArrayList<>();
+			for (Operations ol: searchList)
 				// if the element is done...
-				if (ol.rowIndex==0 && ol.colIndex==0) {
+				if (ol.rowIndex==0 && ol.colIndex==0)
 					finalSet.add(ol);
-				} else { // searching...
+				else { // searching...
 					List<Operations> iterResult = getOperations(ol);
-					searchListNext.addAll(iterResult);	
+					searchListNext.addAll(iterResult);
 				}
-			}
 			searchList=searchListNext;
 		}
-		
-		List<String [] > done = new ArrayList<String []>();
-		
+
+		List<String [] > done = new ArrayList<>();
+
 		return done;
-		
+
 	}
-	
-	List<Operations> getOperations (Operations ol) {	
-		List<Operations> result = new ArrayList<Operations>();
-		
+
+	List<Operations> getOperations (final Operations ol) {
+		List<Operations> result = new ArrayList<>();
+
 		int rowIndex=ol.rowIndex;
 		int colIndex=ol.colIndex;
-		
+
 		int l1 = distance.length-1;
 		int l2 = distance[0].length-1;
 
@@ -226,37 +225,34 @@ public class LevenshteinDistanceResult {
 		int diagonal=Math.max(l1, l2);
 		int up =Math.max(l1, l2);
 		int left=Math.max(l1, l2);
-		if (rowIndex>0 && colIndex>0) {
+		if (rowIndex>0 && colIndex>0)
 			diagonal = distance[rowIndex-1][colIndex-1];
-		}
-		
-		if (rowIndex>0) {
+
+		if (rowIndex>0)
 			up = distance[rowIndex-1][colIndex];
-		}
-		if (colIndex>0) {
+		if (colIndex>0)
 			left = distance[rowIndex][colIndex-1];
-		}
-		
+
 		// if there's more than one path to pick, we need to clone the current list of operations.
 		// this is why there is one path handed in, and can be up to 3 paths out.
-		
+
 		int min=Math.min(Math.min(diagonal, up), left);
-		
+
 		if (min==diagonal) {
 			Operations r = ol.clone();
 			r.addOperation("M");
 			r.setXY(rowIndex-1, colIndex-1);
 			result.add(r);
 		}
-		
+
 		if (min==up) {
 			Operations r = ol.clone();
 			r.addOperation("D");
 			r.setXY(rowIndex-1, colIndex);
 			result.add(r);
-			
+
 		}
-		
+
 		if (min==left) {
 			Operations r = ol.clone();
 			r.addOperation("I");
@@ -265,35 +261,35 @@ public class LevenshteinDistanceResult {
 		}
 		return (result);
 	}
-	
+
 	private class Operations {
 		private int rowIndex;
 		private int colIndex;
 		private List<String> operations;
-		
-		public Operations(int rowIndex, int colIndex) {
+
+		public Operations(final int rowIndex, final int colIndex) {
 			this.rowIndex=rowIndex;
 			this.colIndex = colIndex;
-			this.operations= new ArrayList<String>();
+			this.operations= new ArrayList<>();
 		}
-		
-		public void setXY (int rowIndex, int colIndex) {
+
+		public void setXY (final int rowIndex, final int colIndex) {
 			this.rowIndex=rowIndex;
 			this.colIndex=colIndex;
 		}
-		
-		public void addOperation(String op) {
+
+		public void addOperation(final String op) {
 			this.operations.add(op);
 		}
-		
+
+		@Override
 		public Operations clone () {
 			Operations r = new Operations(this.rowIndex, this.colIndex);
-			for (String o: operations) {
+			for (String o: operations)
 				r.addOperation(new String (o));
-			}
 			return (r);
 		}
-		
+
 		/**
 		 * Operations are returned in the reverse order that they were generated.
 		 * You iterate from the end of the strings to the beginning to trace back the edit distance operations
@@ -310,7 +306,8 @@ public class LevenshteinDistanceResult {
 			}
 			return r;
 		}
-		
+
+		@Override
 		public String toString () {
 			StringBuilder b = new StringBuilder();
 			b.append("[" + this.rowIndex+","+this.colIndex+"]" );
@@ -318,33 +315,31 @@ public class LevenshteinDistanceResult {
 			b.append(h);
 			return b.toString();
 		}
-		
+
 		@Override
         public boolean equals(final Object o) {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
 
             final Operations that = (Operations) o;
-            
+
             if (this.operations.size()!= that.operations.size()) return false;
-            for (int i=0; i<this.operations.size(); i++) {
-            	if (!this.operations.get(i).equals(that.operations.get(i))) return false;
-            }
+            for (int i=0; i<this.operations.size(); i++)
+				if (!this.operations.get(i).equals(that.operations.get(i))) return false;
             return true;
         }
 
         @Override
         public int hashCode() {
         	int result = 31;
-        	for (String s: this.operations) {
-        		result = 31 * result + s.hashCode();
-        	}
+        	for (String s: this.operations)
+				result = 31 * result + s.hashCode();
             return result;
         }
-		
+
 	}
-	
-	
-	
- 	
+
+
+
+
 }
--- src/tests/java/org/broadinstitute/dropseqrna/utils/editdistance/ModifiedLevenshteinDistanceTest.java
+++ src/tests/java/org/broadinstitute/dropseqrna/utils/editdistance/ModifiedLevenshteinDistanceTest.java
@@ -1,63 +1,61 @@
 package org.broadinstitute.dropseqrna.utils.editdistance;
 
 import org.apache.commons.lang.StringUtils;
-import org.broadinstitute.dropseqrna.utils.editdistance.LevenshteinDistance;
-import org.broadinstitute.dropseqrna.utils.editdistance.LevenshteinDistanceResult;
 import org.junit.Assert;
 import org.testng.annotations.Test;
 
 public class ModifiedLevenshteinDistanceTest {
-	
+
 	@Test(enabled=true, groups={"transcriptome", "dropseq"})
 	public void websiteTest () {
 		// see http://stackoverflow.com/questions/5849139/levenshtein-distance-inferring-the-edit-operations-from-the-matrix
-		
+
 		LevenshteinDistanceResult r =  LevenshteinDistance.computeLevenshteinDistanceResult("democrat", "republican");
 		int ed = r.getEditDistance();
 		Assert.assertEquals(8, ed);
 		String [] operations = r.getOperations();
 		String result = StringUtils.join(operations);
 		Assert.assertEquals(result, "SMIISSSSMS");
-		
+
 	}
-	
+
 	@Test(enabled=true, groups={"transcriptome", "dropseq"})
 	public void simpleTests1 () {
 		LevenshteinDistanceResult r =  LevenshteinDistance.computeLevenshteinDistanceResult("AAA", "AAA");
 		int ed = r.getEditDistance();
 		Assert.assertEquals(0, ed);
-		
+
 		r =  LevenshteinDistance.computeLevenshteinDistanceResult("AAA", "ATA");
 		ed = r.getEditDistance();
 		Assert.assertEquals(1, ed);
-		
+
 		r =  LevenshteinDistance.computeLevenshteinDistanceResult("AAA", "ATA", 1,1,2);
 		ed = r.getEditDistance();
 		Assert.assertEquals(2, ed);
-		
+
 		// test deletion
 		r =  LevenshteinDistance.computeLevenshteinDistanceResult("AATA", "AAA");
 		ed = r.getEditDistance();
 		Assert.assertEquals(1, ed);
-		
+
 		// test insertion
 		r =  LevenshteinDistance.computeLevenshteinDistanceResult("AAA", "AATA");
 		ed = r.getEditDistance();
 		Assert.assertEquals(1, ed);
-		
+
 		// test deletion - modify deletion weight
 		r =  LevenshteinDistance.computeLevenshteinDistanceResult("AATA", "AAA", 2,1,1);
 		ed = r.getEditDistance();
 		Assert.assertEquals(2, ed);
-				
+
 		// test insertion - modify insertion weight
 		r =  LevenshteinDistance.computeLevenshteinDistanceResult("AAA", "AATA", 1,2,1);
 		ed = r.getEditDistance();
 		Assert.assertEquals(2, ed);
-		
-		
+
+
 	}
-	
+
 	@Test(enabled=true, groups={"transcriptome", "dropseq"})
 	public void testEditDistanceOperations () {
 		LevenshteinDistanceResult r =  LevenshteinDistance.computeLevenshteinDistanceResult("AAA", "AATA");
@@ -66,63 +64,81 @@ public class ModifiedLevenshteinDistanceTest {
 		String [] operations = r.getOperations();
 		String result = StringUtils.join(operations);
 		Assert.assertEquals("MMIM", result);
-		
+
 	}
-	
-	
+
+
 	@Test(enabled=true, groups={"transcriptome", "dropseq"})
 	public void testEditDistanceOperationsFull () {
-		
+
 		testEditDistance("AGGCTAGGTGTT","AGGCTAAGGTGT", 1);
 		testEditDistance("AGGCTAGGTGTT","AGCTAGGTGTTT",1);
 		testEditDistance("AGGCTAGGTGTT","AGCTAGCTGTTT",2);
-		
+
 		// 1 indel, 2 subs
 		testEditDistance("AGGCTAGGTGTT","ACCTAGCTGTTT",3);
-		
+
 		// 1 indel, 0 subs
 		testEditDistance("AAAGAGGACGG","AAGAGGACGCG",1);
-				
-		
+
+
 	}
-	
+
 	@Test(enabled=true, groups={"transcriptome", "dropseq"})
 	public void testEditDistanceOperationsFull2 () {
-		
+
 		// 2 bp insertion
 		testEditDistance("GCT", "TAG", 2);
-		
+
 		// 2 bp substitution
 		testEditDistance("AATG","ZZTG",2);
-		
+
 		// 2 bp insertion of a "GC"
 		testEditDistance("TCCCAGCAATAG","TCCCAGCAAGCT",2);
-		
+
 		// has an indel of length 1 and a substitution of the first base.
 		testEditDistance("TATTTTACGCAG","AATTTTAACGCA",2);
-		
+
 		// 2 bp insertion, 1 substiution
 		testEditDistance("ACCCAGCAATAG","TCCCAGCAAGCT",3);
-		
+
 		// go bigger.
-		
-		// 4 bp insertion, 2 substiution.  Turns out this is ED=4, 'cause long runs of a base "lose" info. 
+
+		// 4 bp insertion, 2 substiution.  Turns out this is ED=4, 'cause long runs of a base "lose" info.
 		testEditDistance("TCCCAGCAAGCT","TCCAAAACATTA",4);
-		
+
 		// 3 bp insertion, 3bp substitution.
 		testEditDistance("AAACCATTTGGG","GAGCCCCCCTTT",6);
-		
-		
-		
+
+
+
 	}
-	
-	private void testEditDistance (String a, String b, int expectedED) {
+
+	private void testEditDistance (final String a, final String b, final int expectedED) {
 		LevenshteinDistanceResult r = LevenshteinDistance.computeLevenshteinDistanceResult(a,b, 1,1,2);
-		
+
 		int ed2 = r.getEditDistanceIndelCorrected();
 		Assert.assertEquals(expectedED, ed2);
-		
+
 	}
-	
-	
+
+	@Test(enabled=true)
+	public void testIndelOnly() {
+		String bc1="AAAAGTGAGGAC";
+		String [] bc2 ={"AAAAGTGAGGCA", "AAAAGTGAGGCC", "AAAAGTGAGGCG", "AAAAGTGAGGCT"};
+		String [] opsExpected={"M", "M", "M", "M", "M", "M", "M", "M", "M", "M", "D", "M", "I"};
+		for (String element : bc2) {
+			LevenshteinDistanceResult r = LevenshteinDistance.computeLevenshteinDistanceResult(bc1,element, 1,1,2);
+			int ed1 = r.getEditDistance();
+			int ed11= r.getEditDistanceIndelCorrected();
+
+			Assert.assertEquals(ed1, 2);
+			Assert.assertEquals(ed11, 1);
+			String []  ops = r.getOperations();
+			for (int i=0; i<ops.length; i++)
+				Assert.assertEquals(opsExpected[i], ops[i]);
+		}
+	}
+
+
 }
