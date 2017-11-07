75efb246d38b6e774d04c05eb3f7ebc3a118faeb
Alec Wysoker <alecw@broadinstitute.org>
2017-11-07 11:25:56 -0500

    Add license to java programs.

--- src/java/org/broadinstitute/dropseqrna/barnyard/SingleCellRnaSeqMetricsCollector.java
+++ src/java/org/broadinstitute/dropseqrna/barnyard/SingleCellRnaSeqMetricsCollector.java
@@ -1,3 +1,13 @@
+/*
+ * The Broad Institute
+ * SOFTWARE COPYRIGHT NOTICE AGREEMENT
+ * This software and its documentation are copyright 2017 by the
+ * Broad Institute/Massachusetts Institute of Technology. All rights are reserved.
+ *
+ * This software is supplied without any warranty or guaranteed support whatsoever.
+ * Neither the Broad Institute nor MIT can be responsible for its use, misuse,
+ * or functionality.
+ */
 package org.broadinstitute.dropseqrna.barnyard;
 
 import java.io.BufferedWriter;
