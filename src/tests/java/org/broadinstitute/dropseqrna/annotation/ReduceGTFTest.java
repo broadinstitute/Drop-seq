ac4788e3fd6ec55d1b697a1929cc3595bc3832d8
jamesnemesh <nemesh@broadinstitute.org>
2018-09-06 16:01:44 -0400

    ReduceGTF additional test coverage

--- src/tests/java/org/broadinstitute/dropseqrna/annotation/ReduceGTFTest.java
+++ src/tests/java/org/broadinstitute/dropseqrna/annotation/ReduceGTFTest.java
@@ -23,14 +23,17 @@
  */
 package org.broadinstitute.dropseqrna.annotation;
 
-import htsjdk.samtools.util.CollectionUtil;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
 import java.io.File;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import htsjdk.samtools.util.CollectionUtil;
 
 
 public class ReduceGTFTest {
@@ -42,8 +45,10 @@ public class ReduceGTFTest {
 	File GTF_FILE4 = new File("testdata/org/broadinstitute/transcriptome/annotation/human_APITD1.gtf.gz");
 	// contains both the APITD1 gene and the APITD1-CORT genes, that occupy the same bounds, but have different sets of exons.
 	File GTF_FILE5 = new File("testdata/org/broadinstitute/transcriptome/annotation/human_APITD1_both.gtf.gz");
-	
+	File GTF_FILE5_REDUCED= new File("testdata/org/broadinstitute/transcriptome/annotation/human_APITD1_both.gtf.reduced.gz");
+
 	File SD = new File("testdata/org/broadinstitute/transcriptome/annotation/human_g1k_v37_decoy_50.dict");
+
 
 
     private Iterator<GTFRecord> parseGtf(final File file) {
@@ -51,14 +56,58 @@ public class ReduceGTFTest {
         r.SEQUENCE_DICTIONARY = SD;
         r.GTF = file;
         return r.parseGTF();
+
+
+
     }
-	
+
+    @Test
+    public void testDoWork () {
+    	ReduceGTF r = new ReduceGTF();
+    	r.GTF=GTF_FILE5;
+    	r.SEQUENCE_DICTIONARY=SD;
+    	File o = getTempReportFile("ReduceGTF", ".reduced.gtf.gz");
+    	// File o2 = getTempReportFile("ReduceGTF", ".reduced.gtf");
+    	o.deleteOnExit();
+    	// o2.deleteOnExit();
+    	//r.OUTPUT=GTF_FILE5_REDUCED;
+    	r.OUTPUT=o;
+    	r.ENHANCE_GTF=true;
+    	int ret = r.doWork();
+    	Assert.assertTrue(ret==0);
+//    	r.OUTPUT=o2;
+//    	int ret2 = r.doWork();
+//    	Assert.assertTrue(ret2==0);
+
+
+    	try {
+			boolean t1 = FileUtils.contentEquals(o, GTF_FILE5_REDUCED);
+			Assert.assertTrue(t1);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
+    }
+
+    private File getTempReportFile (final String prefix, final String suffix) {
+		File tempFile=null;
+
+		try {
+			tempFile = File.createTempFile(prefix, suffix);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		return tempFile;
+	}
+
+
 	@Test(enabled=true, groups={"dropseq", "transcriptome"})
 	public void test1() {
 		Assert.assertNotNull(parseGtf(GTF_FILE1));
-		
+
 	}
-	
+
 	@Test(enabled=true, groups={"dropseq", "transcriptome"})
 	public void test2() {
 		final Iterator<GTFRecord> gtfIterator = parseGtf(GTF_FILE3);
@@ -66,12 +115,12 @@ public class ReduceGTFTest {
 		EnhanceGTFRecords e = new EnhanceGTFRecords();
 		List<GTFRecord> records = e.enhanceGTFRecords(gtfIterator);
 		Assert.assertNotNull(records);
-		
+
 		for (GTFRecord a: records) {
 			Assert.assertEquals(a.getStart(), 152252);
 			Assert.assertEquals(a.getEnd(), 152312);
 			Assert.assertEquals(a.getStrandAsString(), "+");
-			
+
 			if (a.getFeatureType().equals("gene")) {
 				Assert.assertEquals(a.getTranscriptID(), null);
 				Assert.assertEquals(a.getTranscriptName(), null);
@@ -80,11 +129,11 @@ public class ReduceGTFTest {
 				Assert.assertEquals(a.getTranscriptID(), "ENST00000577630");
 				Assert.assertEquals(a.getTranscriptName(), "AL592188.5-201");
 			}
-			
+
 		}
-		
+
 	}
-	
+
 	@Test(enabled=true, groups={"dropseq", "transcriptome"})
 	public void testAPITD1() {
 		Iterator<GTFRecord> gtfIterator = parseGtf(GTF_FILE4);
@@ -98,11 +147,10 @@ public class ReduceGTFTest {
 		Assert.assertEquals(genes.size(),1);
 
         final EnhanceGTFRecords enhancer = new EnhanceGTFRecords();
-        for (final GeneFromGTF gene : genes) {
-            Assert.assertNotNull(enhancer.enhanceGene(gene));
-        }
+        for (final GeneFromGTF gene : genes)
+			Assert.assertNotNull(enhancer.enhanceGene(gene));
 	}
-	
+
 	@Test(enabled=true, groups={"dropseq", "transcriptome"})
 	public void testAPITD1Complex() {
         Iterator<GTFRecord> gtfIterator = parseGtf(GTF_FILE5);
@@ -116,8 +164,7 @@ public class ReduceGTFTest {
         Assert.assertEquals(genes.size(),2);
 
         final EnhanceGTFRecords enhancer = new EnhanceGTFRecords();
-        for (final GeneFromGTF gene : genes) {
-            Assert.assertNotNull(enhancer.enhanceGene(gene));
-        }
+        for (final GeneFromGTF gene : genes)
+			Assert.assertNotNull(enhancer.enhanceGene(gene));
 	}
 }
