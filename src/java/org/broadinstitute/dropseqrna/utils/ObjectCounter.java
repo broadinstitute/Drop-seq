3b71dd5409cf6fea1b70c16821f8dafe362e0712
jamesnemesh <nemesh@broadinstitute.org>
2018-09-20 16:17:05 -0400

    added equals/hashmap to ObjectCounter (#31)

--- src/java/org/broadinstitute/dropseqrna/utils/ObjectCounter.java
+++ src/java/org/broadinstitute/dropseqrna/utils/ObjectCounter.java
@@ -210,11 +210,34 @@ public class ObjectCounter<T extends Comparable<T>> {
 		this.countMap=result;
 	}
 
-	// public boolean equals()
-
 	@Override
 	public String toString () {
 		return this.countMap.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((countMap == null) ? 0 : countMap.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ObjectCounter other = (ObjectCounter) obj;
+		if (countMap == null) {
+			if (other.countMap != null)
+				return false;
+		} else if (!countMap.equals(other.countMap))
+			return false;
+		return true;
 	}
 
 
