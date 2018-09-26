c345c2368797d283a53e7fe5bfe739b33ccfded2
jamesnemesh <nemesh@broadinstitute.org>
2018-09-26 10:41:40 -0400

    Fix test to refer to proper INTERVALS argument.
    (This got by refactoring because it's a string argument instead of
    referencing a member variable)

--- src/java/org/broadinstitute/dropseqrna/utils/FilterBam.java
+++ src/java/org/broadinstitute/dropseqrna/utils/FilterBam.java
@@ -71,7 +71,7 @@ public class FilterBam extends CommandLineProgram{
 	@Argument(shortName = StandardOptionDefinitions.OUTPUT_SHORT_NAME, doc = "Output report")
 	public File OUTPUT;
 
-	@Argument(shortName="READ_MQ", doc = "Minimum mapping quality to include the read in the analysis.  Reads are not filtered on map quality by default.")
+	@Argument(shortName="READ_MQ", doc = "Minimum mapping quality to include the read in the analysis.  Reads are not filtered on map quality by default.", optional=true)
 	public Integer MINIMUM_MAPPING_QUALITY = null;
 
 	@Argument(doc = "Should PCR duplicates be filtered?", optional=true)
