8759378d55f23998d3986ebf40017bd70a77ce5e
jamesnemesh <nemesh@broadinstitute.org>
2020-11-11 22:24:29 -0500

    Csi now has confidence intervals (#219)
    Updated test expected outputs to include confidence intervals.

--- src/java/org/broadinstitute/dropseqrna/censusseq/CsiMetrics.java
+++ src/java/org/broadinstitute/dropseqrna/censusseq/CsiMetrics.java
@@ -1,6 +1,7 @@
 package org.broadinstitute.dropseqrna.censusseq;
 
 import org.apache.commons.math3.stat.descriptive.moment.Mean;
+import org.broadinstitute.dropseqrna.utils.statistics.BinomialStatistics;
 
 import htsjdk.samtools.metrics.MetricBase;
 
@@ -15,6 +16,8 @@ public class CsiMetrics extends MetricBase {
 	public double FRAC_DOSAGE_ALT;
 	public double SEQUENCING_ERROR_RATE = 0.001;
 	public double PCT_CONTAMINATION;
+	public double PCT_CONTAMINATION_LOWER_BOUND;
+	public double PCT_CONTAMINATION_UPPER_BOUND;
 	// public double OBSERVED_SEQUENCING_ERROR_RATE;
 	// public double PCT_CONTAMINATION_EMPIRIC_ERROR_RATE;
 	// public double PCT_CONTAMINATION_BQ_AWARE;
@@ -44,12 +47,27 @@ public class CsiMetrics extends MetricBase {
 	public double getObservedSequencingErrorRate () {
 		return errorProbMean.getResult();
 	}
+	
+	private double getMetric (double fracAlt) {
+		double m = ((fracAlt - this.SEQUENCING_ERROR_RATE) / this.MEAN_MAF) * 100;
+		if (m<0) m=0;
+		return (m);
+	}
 
 	public void calculateStats() {
 		MEAN_MAF = this.afMean.getResult();		
 		this.FRAC_ALT = getAltFrequencyByCount();			
-		this.PCT_CONTAMINATION = ((this.FRAC_ALT - this.SEQUENCING_ERROR_RATE) / this.MEAN_MAF) * 100;
+		this.PCT_CONTAMINATION = getMetric(this.FRAC_ALT);
 		if (this.PCT_CONTAMINATION<0) this.PCT_CONTAMINATION=0;
+		
+		// add upper and lower bounds if there are counts.
+		if (this.REF_COUNT+this.ALT_COUNT>0) {
+			BinomialStatistics s = new BinomialStatistics(this.ALT_COUNT+this.REF_COUNT, this.ALT_COUNT, 0.95, this.FRAC_ALT);
+			double lowerBound = s.getBinomialConfidenceInterval().getLowerBound();
+			this.PCT_CONTAMINATION_LOWER_BOUND=getMetric(lowerBound);
+			double upperBound=s.getBinomialConfidenceInterval().getUpperBound();
+			this.PCT_CONTAMINATION_UPPER_BOUND=getMetric(upperBound);
+		}		
 		
 		// OBSERVED_SEQUENCING_ERROR_RATE = getObservedSequencingErrorRate();
 		//this.PCT_CONTAMINATION_EMPIRIC_ERROR_RATE = ((this.FRAC_ALT - this.OBSERVED_SEQUENCING_ERROR_RATE)
