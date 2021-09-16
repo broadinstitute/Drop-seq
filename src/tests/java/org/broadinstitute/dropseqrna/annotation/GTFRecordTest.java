cc654dfaf39f6e8e5ef26e9232ea81c5a6f4fd02
Alec Wysoker <alecw@broadinstitute.org>
2021-09-16 14:59:57 -0400

    Clean up assertions

--- src/tests/java/org/broadinstitute/dropseqrna/annotation/GTFRecordTest.java
+++ src/tests/java/org/broadinstitute/dropseqrna/annotation/GTFRecordTest.java
@@ -4,8 +4,7 @@ import java.util.List;
 
 import htsjdk.samtools.util.StringUtil;
 import org.testng.annotations.Test;
-
-import junit.framework.Assert;
+import org.testng.Assert;
 
 public class GTFRecordTest {
 
@@ -23,16 +22,16 @@ public class GTFRecordTest {
 	public void testCompareTo() {
 		GTFRecord r1 = new GTFRecord("1", 1, 10, false, "g1ID", "g1Name", "t1Name", "t1ID", "coding", "exon", 1);
 		GTFRecord r2 = new GTFRecord("1", 1, 10, false, "g1ID", "g1Name", "t1Name", "t1ID", "coding", "exon", 1);
-		Assert.assertTrue(r1.compareTo(r2)==0);
+		Assert.assertEquals(0, r1.compareTo(r2));
 		// only compares on the interval.
 		r2 = new GTFRecord("1", 1, 10, false, "g2ID", "g1Name", "t1Name", "t1ID", "coding", "exon", 1);
-		Assert.assertTrue(r1.compareTo(r2)==0);
+		Assert.assertEquals(0, r1.compareTo(r2));
 		r2 = new GTFRecord("1", 1, 10, false, "g1ID", "g2Name", "t1Name", "t1ID", "coding", "exon", 1);
-		Assert.assertTrue(r1.compareTo(r2)==0);
+		Assert.assertEquals(0, r1.compareTo(r2));
 		r2 = new GTFRecord("1", 1, 10, false, "g1ID", "g1Name", "t2Name", "t1ID", "coding", "exon", 1);
-		Assert.assertTrue(r1.compareTo(r2)==0);
+		Assert.assertEquals(0, r1.compareTo(r2));
 		r2 = new GTFRecord("1", 1, 10, false, "g1ID", "g1Name", "t1Name", "t2ID", "coding", "exon", 1);
-		Assert.assertTrue(r1.compareTo(r2)==0);
+		Assert.assertEquals(0, r1.compareTo(r2));
 		// change up interval.
 
 		r2 = new GTFRecord("2", 1, 10, false, "g1ID", "g1Name", "t1Name", "t1ID", "coding", "exon", 1);
@@ -48,7 +47,7 @@ public class GTFRecordTest {
 		// strand isn't checked as part of this comparison.
 		r2 = new GTFRecord("1", 1, 10, true, "g1ID", "g1Name", "t1Name", "t1ID", "coding", "exon", 1);
 		val = r1.compareTo(r2);
-		Assert.assertTrue(val==0);
+		Assert.assertEquals(0, val);
 
 
 	}
@@ -56,27 +55,26 @@ public class GTFRecordTest {
 	@Test
 	public void testEquals() {
 		GTFRecord r1 = new GTFRecord("1", 1, 10, false, "g1ID", "g1Name", "t1Name", "t1ID", "coding", "exon", 1);
-		GTFRecord r2 = new GTFRecord("1", 1, 10, false, "g1ID", "g1Name", "t1Name", "t1ID", "coding", "exon", 1);
-		Assert.assertTrue(r1.equals(r1));
-		Assert.assertFalse(r1.equals(null));
-		r2 = new GTFRecord("2", 1, 10, false, "g1ID", "g1Name", "t1Name", "t1ID", "coding", "exon", 1);
-		Assert.assertFalse(r1.equals(r2));
+		Assert.assertEquals(r1, r1);
+		Assert.assertNotEquals(null, r1);
+		GTFRecord r2 = new GTFRecord("2", 1, 10, false, "g1ID", "g1Name", "t1Name", "t1ID", "coding", "exon", 1);
+		Assert.assertNotEquals(r1, r2);
 		r2 = new GTFRecord("1", 2, 10, false, "g1ID", "g1Name", "t1Name", "t1ID", "coding", "exon", 1);
-		Assert.assertFalse(r1.equals(r2));
+		Assert.assertNotEquals(r1, r2);
 		r2 = new GTFRecord("1", 1, 11, false, "g1ID", "g1Name", "t1Name", "t1ID", "coding", "exon", 1);
-		Assert.assertFalse(r1.equals(r2));
+		Assert.assertNotEquals(r1, r2);
 		r2 = new GTFRecord("1", 1, 10, false, "g2ID", "g1Name", "t1Name", "t1ID", "coding", "exon", 1);
-		Assert.assertFalse(r1.equals(r2));
+		Assert.assertNotEquals(r1, r2);
 		r2 = new GTFRecord("1", 1, 10, false, "g1ID", "g2Name", "t1Name", "t1ID", "coding", "exon", 1);
-		Assert.assertFalse(r1.equals(r2));
+		Assert.assertNotEquals(r2, r1);
 		r2 = new GTFRecord("1", 1, 10, false, "g1ID", "g1Name", "t2Name", "t1ID", "coding", "exon", 1);
-		Assert.assertFalse(r1.equals(r2));
+		Assert.assertNotEquals(r2, r1);
 		r2 = new GTFRecord("1", 1, 10, false, "g1ID", "g1Name", "t1Name", "t2ID", "coding", "exon", 1);
-		Assert.assertFalse(r1.equals(r2));
+		Assert.assertNotEquals(r2, r1);
 		r2 = new GTFRecord("1", 1, 10, false, "g1ID", "g1Name", "t1Name", "t1ID", "intronic", "exon", 1);
-		Assert.assertFalse(r1.equals(r2));
+		Assert.assertNotEquals(r2, r1);
 		r2 = new GTFRecord("1", 1, 10, false, "g1ID", "g1Name", "t1Name", "t1ID", "coding", "gene", 1);
-		Assert.assertFalse(r1.equals(r2));
+		Assert.assertNotEquals(r2, r1);
 
 
 	}
@@ -97,7 +95,7 @@ public class GTFRecordTest {
 		Assert.assertNull(errors);
 		// Test null names and IDs
 		errors = new GTFRecord("1", 1, 10, false, null, null, null, null, "coding", "exon", 1).validate();
-		Assert.assertEquals(StringUtil.join("\n", errors),4, errors.size());
+		Assert.assertEquals(4, errors.size(), StringUtil.join("\n", errors));
 		// Test partially invalid but fixed.
 		GTFRecord r = new GTFRecord("1", 1, 10, false, null, "g1Name", "t1Name", null, "coding", "exon", 1);
 		r.validate(true);
