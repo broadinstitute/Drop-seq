758bdc30c1f5aee346833f96f34174225dfcc931
jamesnemesh <nemesh@broadinstitute.org>
2022-01-31 09:44:26 -0500

    More unit testing

---
+++ src/tests/java/org/broadinstitute/dropseqrna/barnyard/digitalallelecounts/sampleassignment/GenerateSyntheticDoubletsTest.java
@@ -0,0 +1,11 @@
+package org.broadinstitute.dropseqrna.barnyard.digitalallelecounts.sampleassignment;
+
+import org.testng.annotations.Test;
+
+public class GenerateSyntheticDoubletsTest {
+
+	@Test(enabled=false)
+	public void doWorkTest() {
+		throw new RuntimeException("Test not implemented");
+	}
+}
