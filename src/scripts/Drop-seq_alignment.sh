broadinstitute/Drop-seq

commit 957e98205b4126582457ad4aae68fada9ada2872

Drop bash-isms
Commit 6ba1fbb2a4626cf22b711225b8ea4ca80887fe89 addressed issues
reported by shellcheck respecting the `#!/usr/bin/env bash` shebang.
This commit removes all bash-isms from those scripts such that
shellcheck passes with a plain `#!/bin/sh` shebang.
This increases portability by enabling to run those scripts on bash-less
systems.
Previously used bash-features that had to be worked around are as
follows:
1. `set -o pipefail`: Dropped, since neither script used a single pipe.
2. arrays (`files_to_delete`): Replaced by exploiting `$@` via `set --`.

src/scripts/Drop-seq_alignment.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 # MIT License
 #
 # Copyright 2018 Broad Institute
@@ -21,7 +21,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-source "$(dirname "$0")"/defs.sh
+. "$(dirname "$0")"/defs.sh
 
 outdir=$(pwd)
 genomedir=
@@ -48,8 +48,6 @@ EOF
 }
 
 set -e
-# Fail if any of the commands in a pipeline fails
-set -o pipefail
 
 while getopts ":o:g:r:es:kvbh" options; do
   case $options in
@@ -79,13 +77,13 @@ check_TMPDIR
 TMPDIR=$(mktemp -d)
 echo "Writing temporary files to $TMPDIR" 
 
-if (( $# != 1 ))
+if [ "$#" -ne 1 ]
 then error_exit "Incorrect number of arguments"
 fi
 
 
-if [[ "$star_executable" != "STAR" ]]
-then if [[ ! ( -x $star_executable && -f $star_executable ) ]]
+if [ "$star_executable" != "STAR" ]
+then if [ ! -x "$star_executable" ] || [ ! -f "$star_executable" ]
      then error_exit "STAR executable $star_executable passed via -s does not exist or is not executable"
      fi
 elif which STAR > /dev/null
@@ -101,8 +99,32 @@ unmapped_bam=$1
 tagged_unmapped_bam=${TMPDIR}/unaligned_mc_tagged_polyA_filtered.bam
 aligned_sam=${TMPDIR}/star.Aligned.out.sam
 aligned_sorted_bam=${TMPDIR}/aligned.sorted.bam
-files_to_delete=()
 
+# Setup intermediate file cleanup
+
+set --
+# shellcheck disable=SC2142 # This is just for readability.
+alias mark_file_as_intermediate='set -- "$@" '
+# shellcheck disable=SC2317 # This function is called via a trap (see below).
+cleanup_intermediates() {
+    exit_code=$?
+    if [ "$keep_intermediates" -eq 1 ]
+    then
+      [ "$verbose" -ne 1 ] || echo "Keeping intermediate files."
+      exit "$exit_code"
+    fi
+    if [ "$exit_code" -ne 0 ]
+    then
+      [ "$verbose" -ne 1 ] || echo "An error occurred...keeping intermediate files."
+      exit "$exit_code"
+    fi
+    [ "$verbose" -ne 1 ] || echo "Finished successfully...removing intermediate files..."
+    for intermediate_file do
+      [ "$verbose" -ne 1 ] || echo "...removing intermediate file '$intermediate_file'."
+      rm -- "$intermediate_file"
+    done
+}
+trap 'cleanup_intermediates "$@"' EXIT
 
 # Stage 1: pre-alignment tag and trim
 
@@ -110,75 +132,71 @@ files_to_delete=()
 invoke_dropseq TagBamWithReadSequenceExtended SUMMARY="${outdir}"/unaligned_tagged_Cellular.bam_summary.txt \
   BASE_RANGE=1-12 BASE_QUALITY=10 BARCODED_READ=1 DISCARD_READ=false TAG_NAME=XC NUM_BASES_BELOW_QUALITY=1 \
   INPUT="${unmapped_bam}" OUTPUT="$TMPDIR"/unaligned_tagged_Cell.bam
-files_to_delete+=("$TMPDIR/unaligned_tagged_Cell.bam")
+  mark_file_as_intermediate "$TMPDIR"/unaligned_tagged_Cell.bam
 
 # molecular tag
 invoke_dropseq TagBamWithReadSequenceExtended SUMMARY="${outdir}"/unaligned_tagged_Molecular.bam_summary.txt \
   BASE_RANGE=13-20 BASE_QUALITY=10 BARCODED_READ=1 DISCARD_READ=true TAG_NAME=XM NUM_BASES_BELOW_QUALITY=1 \
   INPUT="$TMPDIR"/unaligned_tagged_Cell.bam OUTPUT="$TMPDIR"/unaligned_tagged_CellMolecular.bam
-files_to_delete+=("$TMPDIR/unaligned_tagged_CellMolecular.bam")
+  mark_file_as_intermediate "$TMPDIR"/unaligned_tagged_CellMolecular.bam
 
 # quality filter
 invoke_dropseq FilterBam TAG_REJECT=XQ INPUT="$TMPDIR"/unaligned_tagged_CellMolecular.bam \
   OUTPUT="$TMPDIR"/unaligned_tagged_filtered.bam
-files_to_delete+=("$TMPDIR/unaligned_tagged_filtered.bam")
+  mark_file_as_intermediate "$TMPDIR"/unaligned_tagged_filtered.bam
 
 # read trimming
 invoke_dropseq TrimStartingSequence OUTPUT_SUMMARY="${outdir}"/adapter_trimming_report.txt \
   SEQUENCE=AAGCAGTGGTATCAACGCAGAGTGAATGGG MISMATCHES=0 NUM_BASES=5 INPUT="$TMPDIR"/unaligned_tagged_filtered.bam \
   OUTPUT="$TMPDIR"/unaligned_tagged_trimmed_smart.bam
-files_to_delete+=("$TMPDIR/unaligned_tagged_trimmed_smart.bam")
+  mark_file_as_intermediate "$TMPDIR"/unaligned_tagged_trimmed_smart.bam
 
 invoke_dropseq PolyATrimmer OUTPUT="${tagged_unmapped_bam}" OUTPUT_SUMMARY="${outdir}"/polyA_trimming_report.txt \
   MISMATCHES=0 NUM_BASES=6 NEW=true INPUT="$TMPDIR"/unaligned_tagged_trimmed_smart.bam
-files_to_delete+=("${tagged_unmapped_bam}")
+  mark_file_as_intermediate "${tagged_unmapped_bam}"
 
 
 # Stage 2: alignment
 invoke_picard SamToFastq INPUT="${TMPDIR}"/unaligned_mc_tagged_polyA_filtered.bam \
   FASTQ="$TMPDIR"/unaligned_mc_tagged_polyA_filtered.fastq
-files_to_delete+=("$TMPDIR/unaligned_mc_tagged_polyA_filtered.fastq")
+  mark_file_as_intermediate "$TMPDIR"/unaligned_mc_tagged_polyA_filtered.fastq
 
 $ECHO "$star_executable" --genomeDir "${genomedir}" --outFileNamePrefix "${TMPDIR}"/star. \
   --readFilesIn "$TMPDIR"/unaligned_mc_tagged_polyA_filtered.fastq
-files_to_delete+=("${aligned_sam}")
+  mark_file_as_intermediate "${aligned_sam}"
 
 # Stage 3: sort aligned reads (STAR does not necessarily emit reads in the same order as the input)
 invoke_picard \
   SortSam INPUT="${aligned_sam}" OUTPUT="${aligned_sorted_bam}" SORT_ORDER=queryname TMP_DIR="${TMPDIR}"
-files_to_delete+=("${aligned_sorted_bam}")
+  mark_file_as_intermediate "${aligned_sorted_bam}"
 
 # Stage 4: merge and tag aligned reads
 invoke_picard MergeBamAlignment REFERENCE_SEQUENCE="${reference}" UNMAPPED_BAM="${tagged_unmapped_bam}" \
   ALIGNED_BAM="${aligned_sorted_bam}" INCLUDE_SECONDARY_ALIGNMENTS=false PAIRED_RUN=false CLIP_ADAPTERS=false \
   TMP_DIR="${TMPDIR}" OUTPUT="$TMPDIR"/merged.bam
-files_to_delete+=("$TMPDIR/merged.bam")
+  mark_file_as_intermediate "$TMPDIR"/merged.bam
 
 invoke_dropseq TagReadWithInterval I="$TMPDIR"/merged.bam O="$TMPDIR"/gene_tagged.bam TMP_DIR="${TMPDIR}" \
   INTERVALS="${gene_intervals}" TAG=XG
-files_to_delete+=("$TMPDIR/gene_tagged.bam")
+  mark_file_as_intermediate "$TMPDIR"/gene_tagged.bam
 
 invoke_dropseq TagReadWithGeneFunction O="${TMPDIR}"/function_tagged.bam ANNOTATIONS_FILE="${refflat}" \
   INPUT="$TMPDIR"/gene_tagged.bam
 
 if [ "$bead_repair" -ne 0 ]
 then
-  files_to_delete+=("$TMPDIR/function_tagged.bam")
+  mark_file_as_intermediate "$TMPDIR"/function_tagged.bam
 
   # Stage 5: bead repair
   invoke_dropseq DetectBeadSubstitutionErrors INPUT="${TMPDIR}"/function_tagged.bam OUTPUT="${TMPDIR}"/substitution_repaired.bam \
     TMP_DIR="$TMPDIR" MIN_UMIS_PER_CELL=20 OUTPUT_REPORT="${outdir}"/substitution_error_report.txt
-  files_to_delete+=("${TMPDIR}/substitution_repaired.bam")
+  mark_file_as_intermediate "${TMPDIR}"/substitution_repaired.bam
 
   invoke_dropseq DetectBeadSynthesisErrors INPUT="${TMPDIR}"/substitution_repaired.bam MIN_UMIS_PER_CELL=20 \
     OUTPUT_STATS="${outdir}"/synthesis_error_stats.txt SUMMARY="${outdir}"/synthesis_error_summary.txt \
     REPORT="${outdir}"/synthesis_error_report.txt CREATE_INDEX=true TMP_DIR="$TMPDIR" OUTPUT="$outdir"/final.bam
 else
   $ECHO mv "$TMPDIR"/function_tagged.bam "$outdir"/final.bam
-fi
-
-if [ "$keep_intermediates" -eq 0 ]
-then $ECHO rm "${files_to_delete[@]}"
 fi
 
 echo "Completed successfully."

src/scripts/create_Drop-seq_reference_metadata.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 # MIT License
 #
 # Copyright 2018 Broad Institute
@@ -21,10 +21,10 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-source "$(dirname "$0")"/defs.sh
+. "$(dirname "$0")"/defs.sh
 
 progname=$(basename "$0")
-function usage () {
+usage () {
     cat >&2 <<EOF
 USAGE: $progname [options]
 Create Drop-seq reference metadata bundle
@@ -57,8 +57,6 @@ star_executable=$(which STAR 2> /dev/null)
 samtools_executable=$(which samtools 2> /dev/null)
 bgzip_executable=$(which bgzip 2> /dev/null)
 set -e
-# Fail if any of the commands in a pipeline fails
-set -o pipefail
 
 
 

commit d2bdf19707c17a427d1812e9e279128cc3fcc278

Fix indentation errors introduced by 957e982
Commit 957e98205b4126582457ad4aae68fada9ada2872 introduced the
`mark_file_as_intermediate` convenience-alias to `Drop-seq_alignment.sh`
to work around the lack of arrays in POSIX shells.
To reduce the risk of breaking anything due to this change, regular
expression replacement was used to adjust the code accordingly.
By mistake, this introduced a level of indentation to *all* calls of
that alias, not only the one instance wrapped into a conditional block.
While this is only a cosmetic issue that has no functional consequences,
correcting the indentation of the affected lines improves readability
and thus maintainability of the code.

src/scripts/Drop-seq_alignment.sh
@@ -135,55 +135,55 @@ trap 'cleanup_intermediates "$@"' EXIT
 invoke_dropseq TagBamWithReadSequenceExtended SUMMARY="$outdir"/unaligned_tagged_Cellular.bam_summary.txt \
   BASE_RANGE=1-12 BASE_QUALITY=10 BARCODED_READ=1 DISCARD_READ=false TAG_NAME=XC NUM_BASES_BELOW_QUALITY=1 \
   INPUT="$unmapped_bam" OUTPUT="$TMPDIR"/unaligned_tagged_Cell.bam
-  mark_file_as_intermediate "$TMPDIR"/unaligned_tagged_Cell.bam
+mark_file_as_intermediate "$TMPDIR"/unaligned_tagged_Cell.bam
 
 # molecular tag
 invoke_dropseq TagBamWithReadSequenceExtended SUMMARY="$outdir"/unaligned_tagged_Molecular.bam_summary.txt \
   BASE_RANGE=13-20 BASE_QUALITY=10 BARCODED_READ=1 DISCARD_READ=true TAG_NAME=XM NUM_BASES_BELOW_QUALITY=1 \
   INPUT="$TMPDIR"/unaligned_tagged_Cell.bam OUTPUT="$TMPDIR"/unaligned_tagged_CellMolecular.bam
-  mark_file_as_intermediate "$TMPDIR"/unaligned_tagged_CellMolecular.bam
+mark_file_as_intermediate "$TMPDIR"/unaligned_tagged_CellMolecular.bam
 
 # quality filter
 invoke_dropseq FilterBam TAG_REJECT=XQ INPUT="$TMPDIR"/unaligned_tagged_CellMolecular.bam \
   OUTPUT="$TMPDIR"/unaligned_tagged_filtered.bam
-  mark_file_as_intermediate "$TMPDIR"/unaligned_tagged_filtered.bam
+mark_file_as_intermediate "$TMPDIR"/unaligned_tagged_filtered.bam
 
 # read trimming
 invoke_dropseq TrimStartingSequence OUTPUT_SUMMARY="$outdir"/adapter_trimming_report.txt \
   SEQUENCE=AAGCAGTGGTATCAACGCAGAGTGAATGGG MISMATCHES=0 NUM_BASES=5 INPUT="$TMPDIR"/unaligned_tagged_filtered.bam \
   OUTPUT="$TMPDIR"/unaligned_tagged_trimmed_smart.bam
-  mark_file_as_intermediate "$TMPDIR"/unaligned_tagged_trimmed_smart.bam
+mark_file_as_intermediate "$TMPDIR"/unaligned_tagged_trimmed_smart.bam
 
 invoke_dropseq PolyATrimmer OUTPUT="$tagged_unmapped_bam" OUTPUT_SUMMARY="$outdir"/polyA_trimming_report.txt \
   MISMATCHES=0 NUM_BASES=6 NEW=true INPUT="$TMPDIR"/unaligned_tagged_trimmed_smart.bam
-  mark_file_as_intermediate "$tagged_unmapped_bam"
+mark_file_as_intermediate "$tagged_unmapped_bam"
 
 
 # Stage 2: alignment
 invoke_picard SamToFastq INPUT="$TMPDIR"/unaligned_mc_tagged_polyA_filtered.bam \
   FASTQ="$TMPDIR"/unaligned_mc_tagged_polyA_filtered.fastq
-  mark_file_as_intermediate "$TMPDIR"/unaligned_mc_tagged_polyA_filtered.fastq
+mark_file_as_intermediate "$TMPDIR"/unaligned_mc_tagged_polyA_filtered.fastq
 
 $ECHO "$star_executable" --genomeDir "$genomedir" --outFileNamePrefix "$TMPDIR"/star. \
   --readFilesIn "$TMPDIR"/unaligned_mc_tagged_polyA_filtered.fastq --runThreadN "$ncores"
-  mark_file_as_intermediate "$aligned_sam"
+mark_file_as_intermediate "$aligned_sam"
 
 $ECHO mv "$TMPDIR"/star.Log.final.out "$outdir"
 
 # Stage 3: sort aligned reads (STAR does not necessarily emit reads in the same order as the input)
 invoke_picard \
   SortSam INPUT="$aligned_sam" OUTPUT="$aligned_sorted_bam" SORT_ORDER=queryname TMP_DIR="$TMPDIR"
-  mark_file_as_intermediate "$aligned_sorted_bam"
+mark_file_as_intermediate "$aligned_sorted_bam"
 
 # Stage 4: merge and tag aligned reads
 invoke_picard MergeBamAlignment REFERENCE_SEQUENCE="$reference" UNMAPPED_BAM="$tagged_unmapped_bam" \
   ALIGNED_BAM="$aligned_sorted_bam" INCLUDE_SECONDARY_ALIGNMENTS=false PAIRED_RUN=false CLIP_ADAPTERS=false \
   TMP_DIR="$TMPDIR" OUTPUT="$TMPDIR"/merged.bam
-  mark_file_as_intermediate "$TMPDIR"/merged.bam
+mark_file_as_intermediate "$TMPDIR"/merged.bam
 
 invoke_dropseq TagReadWithInterval I="$TMPDIR"/merged.bam O="$TMPDIR"/gene_tagged.bam TMP_DIR="$TMPDIR" \
   INTERVALS="$gene_intervals" TAG=XG
-  mark_file_as_intermediate "$TMPDIR"/gene_tagged.bam
+mark_file_as_intermediate "$TMPDIR"/gene_tagged.bam
 
 invoke_dropseq TagReadWithGeneFunction O="$TMPDIR"/function_tagged.bam ANNOTATIONS_FILE="$refflat" \
   INPUT="$TMPDIR"/gene_tagged.bam