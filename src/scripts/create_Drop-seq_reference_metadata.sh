broadinstitute/Drop-seq

commit 745540f076c1a8163a069566fd8df6fee244e9a4

Drop empty variable expansion / superfluous space
The `filtered_gene_biotypes` parameter was initialized empty but
expanded upon its (conditional) assignment in a self-appending manner.
Effectively, this only added an extra leading space before the `G`
parameters being set in that line. Since the variable is expanded as
part of a command where it is preceded by a space, the only effect this
self-reference had, was to introduce an extra space that has no effect.
Removing this construct improves readability and thus maintainability of
the code.

src/scripts/create_Drop-seq_reference_metadata.sh
@@ -65,7 +65,7 @@ while getopts ':n:r:s:g:f:o:a:i:b:evh' options; do
     r ) reference_fasta=$OPTARG;;
     s ) species=$OPTARG;;
     g ) gtf=$OPTARG;;
-    f ) filtered_gene_biotypes="$filtered_gene_biotypes G=$OPTARG";;
+    f ) filtered_gene_biotypes="G=$OPTARG";;
     o ) outdir=$OPTARG;;
     a ) star_executable=$OPTARG;;
     i ) samtools_executable=$OPTARG;;

commit e19c07be3c1986f51a9f6fae5ccdb4a2c0ac8e2e

Fix regression introduced by 745540f
Commit 745540f076c1a8163a069566fd8df6fee244e9a4 wrongly removed a
supposedly 'empty' variable expansion along a presumed 'superfluous'
space following it.
However, as pointed out by Alec Wysoker (@alecw) in
https://github.com/broadinstitute/Drop-seq/pull/412#discussion_r1569243481,
those were intentional to enable passing multiple `-f` parameters and
having *all* of them take effect instead of just the last one (implicit
behaviour of all other options and `-f` since commit 745540f).

Instead of simply reverting the erroneous commit, this commit improves
upon the state preceding that commit in two ways:

1. A comment was added to avoid similar confusion in the future.
2. The space separating the `G=<biotype>` parameters generated from
   different `-f <biotype>` options is only added in-between consecutive
   parameters, but not preceding the first one.
   While has no functional consequence it further stressed the fact that
   `filtered_gene_biotypes` might be empty, but might also contain a
   value already (from the second `-f` parameter onwards).

src/scripts/create_Drop-seq_reference_metadata.sh
@@ -66,7 +66,8 @@ while getopts ':n:r:s:g:f:o:a:b:i:veh' options; do
     r ) reference_fasta=$OPTARG;;
     s ) species=$OPTARG;;
     g ) gtf=$OPTARG;;
-    f ) filtered_gene_biotypes="G=$OPTARG";;
+    f ) filtered_gene_biotypes="${filtered_gene_biotypes+$filtered_gene_biotypes }G=$OPTARG";;
+        # support repeating -f: ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
     o ) outdir=$OPTARG;;
     a ) star_executable=$OPTARG;;
     b ) bgzip_executable=$OPTARG;;