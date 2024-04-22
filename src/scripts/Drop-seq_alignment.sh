broadinstitute/Drop-seq

commit f235793fe3ead480d2af9384b3138503eb8ccc05

Shell scripts: Unset uninitialized parameters
Commit a1bc4797dda88121facda747e78f309e3fed7beb removed empty
(initialization) assignments and
3fc809ebd57ba4f73b13e3b9e8489f6131ee32f6 did the same for 'no-op'
unset operations.
The rationale was that those (empty) initializations would no have any
effect (in the context of the script itself) and while the idea of
initializing each and every parameter seemed nice, the state of the code
suggested the maintenance burden was too high.
However, in
https://github.com/broadinstitute/Drop-seq/pull/412#discussion_r1569231368
Alec Wysoker (@alecw) asked to re-instate these empty initializations
'in order to ensure they're passed on command line rather than inherited
from somewhere'.

Instead of simply reverting the removal of the previous empty
initializations, this commit systematically unsets all variables to be
set by command line parameters that have not been initialized before
just before parsing the command line.
Sourced scripts have been considered as well for potential
initializations and comments have been added to document this design
choice.
For consistency, the order of parameters within the (single) unset
statements was chosen to match the order of the parameters (which has
already been standardizes between the usage string and the command line
parsing `case` statements).

Note that capitalized variables have been excluded from this
unconditional empty initialization as an all-capital variable name
usually flags a variable as an environment variable that is meant to be
overridden outside of the script by the shell calling the script.

src/scripts/Drop-seq_alignment.sh
@@ -50,6 +50,17 @@ EOF
 
 set -e
 
+# Unset all variables to be set based on parameters that have not been
+# initialized previously in this script (or a script sourced above) 'in order
+# to ensure they're passed on command line rather than inherited from
+# somewhere'.
+# See https://github.com/broadinstitute/Drop-seq/pull/412#discussion_r1569231368
+# for the corresponding discussion.
+unset \
+  genomedir \
+  reference \
+  verbose
+
 while getopts ':g:r:o:s:n:bekvh' options; do
   case $options in
     g ) genomedir=$OPTARG;;

src/build/make_wrapper_scripts.sh
@@ -55,6 +55,16 @@ check_set() {
 
 set -e
 
+# Unset all variables to be set based on parameters that have not been
+# initialized previously in this script 'in order to ensure they're passed on
+# command line rather than inherited from somewhere'.
+# See https://github.com/broadinstitute/Drop-seq/pull/412#discussion_r1569231368
+# for the corresponding discussion.
+unset \
+  template \
+  classpath \
+  outdir
+
 while getopts ':t:c:d:m:h' options; do
   case $options in
     t ) template=$OPTARG;;

src/build/public_clp_template.sh
@@ -46,6 +46,14 @@ usage () {
 
 set -e
 
+# Note: All variables to be set based on parameters have been initialized
+# previously in this script.
+# If that wasn't the case, all the uninitialized ones should be unset here 'in
+# order to ensure they're passed on command line rather than inherited from
+# somewhere'.
+# See https://github.com/broadinstitute/Drop-seq/pull/412#discussion_r1569231368
+# for the corresponding discussion.
+
 while getopts ':m:vh' options; do
   case $options in
     m ) xmx=$OPTARG;;

src/scripts/create_Drop-seq_reference_metadata.sh
@@ -59,6 +59,18 @@ bgzip_executable=$(which bgzip 2> /dev/null)
 set -e
 
 
+# Unset all variables to be set based on parameters that have not been
+# initialized previously in this script (or a script sourced above) 'in order
+# to ensure they're passed on command line rather than inherited from
+# somewhere'.
+# See https://github.com/broadinstitute/Drop-seq/pull/412#discussion_r1569231368
+# for the corresponding discussion.
+unset \
+  reference_name \
+  reference_fasta \
+  species \
+  gtf \
+  filtered_gene_biotypes
 
 while getopts ':n:r:s:g:f:o:a:b:i:veh' options; do
   case $options in

commit c4b1ba14e4902b05c5e6545f744e8b21afa4ea4d

Fix regression introduced by f235793
Commit f235793fe3ead480d2af9384b3138503eb8ccc05 mistakenly unset the
`verbose` parameter after its (zero) initialization in `defs.sh` leading
to downstream errors.
This commit fixes this. The comment already claimed to consider sourced
files but now this is actually true.

src/scripts/Drop-seq_alignment.sh
@@ -63,8 +63,7 @@ set -e
 # for the corresponding discussion.
 unset \
   genomedir \
-  reference \
-  verbose
+  reference
 
 while getopts ':g:r:o:s:n:bekvh' options; do
   case $options in