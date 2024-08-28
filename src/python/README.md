b062a1937a98f32e5ac40c5f8d0e347a3ae2af5a
Alec Wysoker <alecw@broadinstitute.org>
2024-08-28 10:47:52 -0400

    package python code for hdf5 conversion

---
+++ src/python/README.md
@@ -0,0 +1,1 @@
+# Simple python tools for converting between hdf5 and tabular digital gene expression file formats.
---
+++ src/python/src/dropseq_hdf5/downstream.py
@@ -0,0 +1,151 @@
+"""
+Functions for downstream work with outputs of remove-background.
+Courtesy of Stephen Fleming
+"""
+
+import tables
+import numpy as np
+import scipy.sparse as sp
+from typing import Dict
+
+
+def dict_from_h5(file: str) -> Dict[str, np.ndarray]:
+    """Read in everything from an h5 file and put into a dictionary."""
+    d = {}
+    with tables.open_file(file) as f:
+        # read in everything
+        for array in f.walk_nodes("/", "Array"):
+            d[array.name] = array.read()
+    return d
+
+
+def anndata_from_h5(file: str,
+                    analyzed_barcodes_only: bool = True) -> 'anndata.AnnData':
+    """Load an output h5 file into an AnnData object for downstream work.
+
+    Args:
+        file: The h5 file
+        analyzed_barcodes_only: False to load all barcodes, so that the size of
+            the AnnData object will match the size of the input raw count matrix.
+            True to load a limited set of barcodes: only those analyzed by the
+            algorithm. This allows relevant latent variables to be loaded
+            properly into adata.obs and adata.obsm, rather than adata.uns.
+
+    Returns:
+        adata: The anndata object, populated with inferred latent variables
+            and metadata.
+
+    """
+
+    try:
+        import anndata
+    except ImportError:
+        raise ImportError('The anndata package must be installed to use the '
+                          'function anndata_from_h5()')
+
+    d = dict_from_h5(file)
+    X = sp.csc_matrix((d.pop('data'), d.pop('indices'), d.pop('indptr')),
+                      shape=d.pop('shape')).transpose().tocsr()
+
+    if analyzed_barcodes_only:
+        if 'barcodes_analyzed_inds' in d.keys():
+            X = X[d['barcodes_analyzed_inds'], :]
+            d['barcodes'] = d['barcodes'][d['barcodes_analyzed_inds']]
+        elif 'barcode_indices_for_latents' in d.keys():
+            X = X[d['barcode_indices_for_latents'], :]
+            d['barcodes'] = d['barcodes'][d['barcode_indices_for_latents']]
+        elif d['latent_cell_probability'].min() > 0.1:
+            # TODO: bit of a hack
+            # filtered file being read, so we don't need to subset
+            pass
+        else:
+            print('Warning: analyzed_barcodes_only=True, but the key '
+                  '"barcodes_analyzed_inds" or "barcode_indices_for_latents" '
+                  'is missing from the h5 file. '
+                  'Will output all barcodes, and proceed as if '
+                  'analyzed_barcodes_only=False')
+
+    # Construct the count matrix.
+    adata = anndata.AnnData(X=X,
+                            obs={'barcode': d.pop('barcodes').astype(str)},
+                            var={'gene_name': (d.pop('gene_names') if 'gene_names' in d.keys()
+                                               else d.pop('name')).astype(str)})
+    adata.obs.set_index('barcode', inplace=True)
+    adata.var.set_index('gene_name', inplace=True)
+
+    # Add other information to the adata object in the appropriate slot.
+    for key, value in d.items():
+        try:
+            value = np.asarray(value)
+            if len(value.shape) == 0:
+                adata.uns[key] = value
+            elif value.shape[0] == X.shape[0]:
+                if (len(value.shape) < 2) or (value.shape[1] < 2):
+                    adata.obs[key] = value
+                else:
+                    adata.obsm[key] = value
+            elif value.shape[0] == X.shape[1]:
+                if value.dtype.name.startswith('bytes'):
+                    adata.var[key] = value.astype(str)
+                else:
+                    adata.var[key] = value
+            else:
+                adata.uns[key] = value
+        except Exception:
+            print('Unable to load data into AnnData: ', key, value, type(value))
+
+    if analyzed_barcodes_only:
+        for col in adata.obs.columns[adata.obs.columns.str.startswith('barcodes_analyzed')
+                                     | adata.obs.columns.str.startswith('barcode_indices')]:
+            try:
+                del adata.obs[col]
+            except Exception:
+                pass
+
+    return adata
+
+
+def load_anndata_from_input_and_output(input_file: str,
+                                       output_file: str,
+                                       analyzed_barcodes_only: bool = True,
+                                       input_layer_key: str = 'cellranger') -> 'anndata.AnnData':
+    """Load remove-background output count matrix into an anndata object,
+    together with remove-background metadata and the raw input counts.
+
+    Args:
+        input_file: Raw h5 file used as input for remove-background.
+        output_file: Output h5 file created by remove-background (can be
+            filtered or not).
+        analyzed_barcodes_only: Argument passed to anndata_from_h5().
+            False to load all barcodes, so that the size of
+            the AnnData object will match the size of the input raw count matrix.
+            True to load a limited set of barcodes: only those analyzed by the
+            algorithm. This allows relevant latent variables to be loaded
+            properly into adata.obs and adata.obsm, rather than adata.uns.
+        input_layer_key: Key of the anndata.layer that is created for the raw
+            input count matrix.
+
+    Return:
+        adata_out: AnnData object with counts before and after remove-background,
+            as well as inferred latent variables from remove-background.
+
+    """
+
+    # Load input data.
+    adata_raw = anndata_from_h5(input_file, analyzed_barcodes_only=False)
+
+    # Load remove-background output data.
+    adata_out = anndata_from_h5(output_file, analyzed_barcodes_only=analyzed_barcodes_only)
+
+    # Subset the raw dataset to the relevant barcodes.
+    adata_raw = adata_raw[adata_out.obs.index]
+
+    # Put count matrices into 'layers' in anndata for clarity.
+    adata_out.layers[input_layer_key] = adata_raw.X.copy()
+    adata_out.layers['cellbender'] = adata_out.X.copy()
+
+    # Pre-compute a bit of metadata.
+    adata_out.var['n_cellranger'] = np.array(adata_out.layers['cellranger'].sum(axis=0)).squeeze()
+    adata_out.var['n_cellbender'] = np.array(adata_out.layers['cellbender'].sum(axis=0)).squeeze()
+
+    return adata_out
