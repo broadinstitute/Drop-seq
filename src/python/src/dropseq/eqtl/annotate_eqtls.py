544c385e29f43ace5288aa9676aa89abbdfbca36
alecw <alecw@users.noreply.github.com>
2025-07-24 08:59:26 -0400

    Script to annotate cis_qtl file with data from GTF and dbSNP (#539)

---
+++ src/python/src/dropseq/eqtl/annotate_eqtls.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+# MIT License
+# 
+# Copyright 2025 Broad Institute
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+"""
+# Annotate eQTLs with information from the GTF, dbSNP, and eventually other sources.
+"""
+
+import argparse
+import sys
+from types import MethodType
+import pandas as pd
+import dropseq.metadata.read_gtf as read_gtf
+
+from dropseq.util.argparse_utils import argparse_error
+import dropseq.util.pandas_utils as pd_utils
+
+def join_gtf(qtls, gtfPath, missing_value):
+    """
+    Annotate eQTLs with gene information from a GTF file.
+    """
+    gtf = read_gtf.read_gtf(gtfPath)
+    # Filter for genes only, and select relevant columns
+    gtf = gtf[gtf.FEATURE == 'gene']
+    gtf = gtf[['gene_name', 'CHROMOSOME', 'START', 'END', 'STRAND', 'gene_id']]
+    # rename columns to match what we want to go into the qtls DataFrame
+    gtf.rename(columns={
+        'CHROMOSOME': 'gene_chr',
+        'START': 'gene_start',
+        'END': 'gene_end',
+        'STRAND': 'strand',
+        'gene_id': 'phenotype_id',  # the join column
+    }, inplace=True)
+    # change these columns to strings so that missing values can be replaced with a string
+    gtf['gene_start'] = gtf['gene_start'].astype(str)
+    gtf['gene_end'] = gtf['gene_end'].astype(str)
+    gtf['strand'] = gtf['strand'].astype(str)
+    qtls = qtls.merge(gtf, on='phenotype_id', how='left')
+    qtls.fillna(missing_value, inplace=True)
+    return qtls
+
+def join_dbsnp(qtls, dbsnpPath, missing_value):
+    colnames = ['CHROM', 'POS', 'ID', 'REF', 'ALT']
+    chromIndex = colnames.index('CHROM')
+    posIndex = colnames.index('POS')
+    idIndex = colnames.index('ID')
+    refIndex = colnames.index('REF')
+    altIndex = colnames.index('ALT')
+    numColsOfInterest = len(colnames)
+    # slurp dbSNP file, parse important columns, and put into a dictionary with variant_id constructed from dbSNP columns.
+    dctDbSnp = {}
+    with open(dbsnpPath, 'r') as f:
+        for line in f:
+            line = line.rstrip("\n")
+            if line.startswith('#'):
+                continue
+            fields = line.split('\t', maxsplit=numColsOfInterest)
+            if len(fields) < numColsOfInterest:
+                raise ValueError(f"Error parsing dbSNP file {dbsnpPath}.  Not enough column in line '{line}'.")
+            alternateAlleles = fields[altIndex].split(',')
+            for alternateAllele in alternateAlleles:
+                variant_id = ":".join([fields[chromIndex], fields[posIndex], fields[refIndex], alternateAllele])
+                dctDbSnp[variant_id] = fields[idIndex]
+    # add rsids to qtls DataFrame
+    rsids = [dctDbSnp.get(variant_id, missing_value) for variant_id in qtls['variant_id']]
+    qtls['rsid'] = rsids
+    return qtls
+
+
+
+def main(args=None):
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawTextHelpFormatter,
+        allow_abbrev=False,
+    )
+    parser.error = MethodType(argparse_error, parser)
+
+    parser.add_argument(
+        '--input',
+        '-i',
+        required=True,
+        help='Input cis_qtl.txt.gz file produced by tensorQTL.',
+    )
+    parser.add_argument(
+        '--output',
+        '-o',
+        help='Output file. If not specified, output to stdout.',
+    )
+
+    parser.add_argument(
+        '--gtf',
+        '-g',
+        help='GTF file to annotate eQTLs with gene information.',
+    )
+
+    parser.add_argument(
+        '--dbsnp',
+        '-d',
+        help='dbSNP file to annotate eQTLs with SNP information.',
+    )
+    parser.add_argument('--missing-value', '-m', default='.',
+                        help='String to use for missing values, typically rsids, gene_name, gene_start, gene_end. (default: %(default)s)')
+    options = parser.parse_args(args)
+    if not options.gtf and not options.dbsnp:
+        parser.error("At least one of --gtf or --dbsnp must be specified.")
+    if options.output is None:
+        options.output = sys.stdout
+    qtls = pd.read_csv(options.input, sep='\t')
+    if options.gtf:
+        qtls = join_gtf(qtls, options.gtf, options.missing_value)
+    if options.dbsnp:
+        qtls = join_dbsnp(qtls, options.dbsnp, options.missing_value)
+
+    pd_utils.to_tsv(qtls, options.output, index=False)
+    return 0
+
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ src/python/src/dropseq/metadata/read_gtf.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# MIT License
+# 
+# Copyright 2025 Broad Institute
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# Based on https://github.com/openvax/gtfparse
+
+import pandas as pd
+
+gtf_colnames = [
+'CHROMOSOME', 'SOURCE', 'FEATURE', 'START', 'END', 'SCORE', 'STRAND', 'FRAME', 'ATTRIBUTE'
+]
+
+def fix_attribute_field(attribute):
+    """
+    Fix the attribute field in a GTF file by replacing quotes and semicolons.
+
+    Args:
+        attribute (str): The attribute field from a GTF file.
+
+    Returns:
+        str: The fixed attribute field.
+    """
+    # Replace quotes with empty string and semicolons with commas
+    return attribute.replace('"', "'").replace(';\"', '\"').replace(";-", "-")
+
+def parse_attribute_to_dict(attribute):
+    """
+    Parse the attribute field of a GTF file into a dictionary.
+
+    Args:
+        attribute (str): The attribute field from a GTF file.
+
+    Returns:
+        dict: A dictionary with parsed attributes.
+    """
+    # Fix the attribute field
+    fixed_attribute = fix_attribute_field(attribute)
+
+    # Split by semicolon and strip whitespace
+    pairs = [pair.strip() for pair in fixed_attribute.split(';') if pair.strip()]
+
+    # Create a dictionary from the pairs
+    return {key_value.split(' ')[0]: key_value.split(' ')[1].strip("'") for key_value in pairs}
+
+def read_gtf(gtf_path):
+    """
+    Read a GTF file and return a DataFrame with gene information.
+
+    Args:
+        gtf_path (str): Path to the GTF file.
+
+    Returns:
+        pd.DataFrame: DataFrame containing gene information.
+    """
+
+    gtf = pd.read_csv(gtf_path, sep='\t', header=0, index_col=None, names=gtf_colnames, comment='#')
+
+    parsedAttributes = gtf['ATTRIBUTE'].apply(parse_attribute_to_dict)
+    # Create a DataFrame from the parsed attributes
+    attributes_df = pd.DataFrame(parsedAttributes.tolist())
+    # Concatenate the attributes DataFrame with the original GTF DataFrame
+    gtf = pd.concat([gtf, attributes_df], axis=1)
+    # Drop the original ATTRIBUTE column
+    gtf.drop(columns=['ATTRIBUTE'], inplace=True)
+    return gtf
+
+
+
